f23f2049103f09517e816a6c935a441a103d087d
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-11-07 11:45:12 +0500

    11:45

---
+++ imest_app/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Teacher, Student, Test, Question, AnswerOption, StudentAnswer
+
+class AnswerOptionInline(admin.TabularInline):
+    model = AnswerOption
+    extra = 1  # Number of empty forms to display for new options
+    fields = ('text', 'is_correct')  # Include the is_correct field
+    list_display = ('text', 'is_correct')  # Show the field in the inline list
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('user', 'school')
+    search_fields = ('user__username', 'school')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'school', 'classroom')
+    search_fields = ('name', 'surname')
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'teacher', 'classroom', 'start_date', 'duration')
+    list_filter = ('teacher', 'classroom')
+    search_fields = ('title',)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'test', 'difficulty')
+    list_filter = ('test',)
+    inlines = [AnswerOptionInline]  # Allow adding multiple answer options inline
+
+@admin.register(StudentAnswer)
+class StudentAnswerAdmin(admin.ModelAdmin):
+    list_display = ('student', 'test', 'question', 'answer', 'points_awarded')
+    list_filter = ('test', 'student')
+
+    # Добавьте функциональность для подсчета общих баллов для каждого студента
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        # Вы можете добавить дополнительные обработки, если необходимо
+        return qs
---
+++ imest_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImestAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imest_app'
